a06367d0039f963f031952721fe45c13d579de52
diegoduqqe@gmail.com <47634821+dukej10@users.noreply.github.com>
2022-02-28 20:16:36 -0500

    mejora código busqueda categoria, tipo y forma de mostrar en interfaz

--- client/src/app/product/product-home/product-home.component.ts
+++ client/src/app/product/product-home/product-home.component.ts
@@ -1,77 +1,76 @@
-import { Component, OnInit } from '@angular/core';
-import { PropertyModel } from 'src/app/models/product.model';
-import { ProductService } from 'src/app/services/product.service';
-import { Routes, RouterModule } from '@angular/router';
-
+import { Component, OnInit } from "@angular/core";
+import { PropertyModel } from "src/app/models/product.model";
+import { ProductService } from "src/app/services/product.service";
+import { Routes, RouterModule } from "@angular/router";
 
 @Component({
-  selector: 'app-product-home',
-  templateUrl: './product-home.component.html',
-  styleUrls: ['./product-home.component.css']
+  selector: "app-product-home",
+  templateUrl: "./product-home.component.html",
+  styleUrls: ["./product-home.component.css"],
 })
 export class ProductHomeComponent implements OnInit {
-
-  constructor(private pdtService: ProductService) {   }
+  constructor(private pdtService: ProductService) {}
 
   productList: PropertyModel[] = [];
 
-  cList: PropertyModel[] = []
+  cList: PropertyModel[] = [];
 
-    condicion: string = null;
-
+  condicion: string = null;
 
   /* Filtro de búsqueda */
-  condition(condicion){
+  condition(condicion) {
     this.cList = [];
-    if(condicion == "venta"){
-      for(let prop of this.productList){
-        if(prop.category == condicion){
-          this.cList.push(prop);
-        }
+    // if (condicion == "venta") {
+    //   for (let prop of this.productList) {
+    //     if (prop.category == condicion) {
+    //       this.cList.push(prop);
+    //     }
+    //   }
+    // }
+
+    for (let prop of this.productList) {
+      if (prop.category.toUpperCase() == condicion.toUpperCase()) {
+        this.cList.push(prop);
+        console.log("CATEGORY");
       }
-    }
-    if(condicion == "alquiler"){
-      for(let prop of this.productList){
-        if(prop.category == condicion){
-          this.cList.push(prop);
-        }
+      if (prop.tipo.toUpperCase() == condicion.toUpperCase()) {
+        this.cList.push(prop);
       }
     }
 
-    if(condicion == "casa"){
-      for(let prop of this.productList){
-        if(prop.tipo == condicion){
-          this.cList.push(prop);
-        }
-      }
-    }
+    // if (condicion == "casa") {
+    //   for (let prop of this.productList) {
+    //     if (prop.tipo == condicion) {
+    //       this.cList.push(prop);
+    //     }
+    //   }
+    // }
 
-    if(condicion == "apartamento"){
-      for(let prop of this.productList){
-        if(prop.tipo == condicion){
-          this.cList.push(prop);
-        }
-      }
-    }
+    // if (condicion == "apartamento") {
+    //   for (let prop of this.productList) {
+    //     if (prop.tipo == condicion) {
+    //       this.cList.push(prop);
+    //     }
+    //   }
+    // }
 
-    if(condicion == "local"){
-      console.log(condicion);
-      for(let prop of this.productList){
-        if(prop.tipo == condicion){
-          this.cList.push(prop);
-        }
-      }
-    }
-}
-
-  ngOnInit(){
-    this.getAllProducts(); 
+    // if (condicion == "local") {
+    //   console.log(condicion);
+    //   for (let prop of this.productList) {
+    //     if (prop.tipo == condicion) {
+    //       this.cList.push(prop);
+    //     }
+    //   }
+    // }
   }
 
-  getAllProducts():void{
-    this.pdtService.getAllProducts().subscribe(items => {
+  ngOnInit() {
+    this.getAllProducts();
+  }
+
+  getAllProducts(): void {
+    this.pdtService.getAllProducts().subscribe((items) => {
       this.productList = items;
     });
   }
-
 }
