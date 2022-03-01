7d0b76dad802fa52185d165a77187aeeace23d1e
diegoduqqe@gmail.com <47634821+dukej10@users.noreply.github.com>
2022-03-01 16:58:55 -0500

    agregar fecha actual a la solicitud

--- client/src/app/product/product-details/product-details.component.ts
+++ client/src/app/product/product-details/product-details.component.ts
@@ -1,30 +1,35 @@
-import { Component, OnInit } from '@angular/core';
-import { ProductModule } from '../product.module';
-import { ProductService } from 'src/app/services/product.service';
-import { ActivatedRoute } from '@angular/router';
-import { PropertyModel } from 'src/app/models/product.model';
-import { SolicitModel } from 'src/app/models/solicitd.model';
-import { SolicitudService } from 'src/app/services/solicitud.service';
-import { UserService } from 'src/app/services/user.service';
+import { Component, OnInit } from "@angular/core";
+import { ProductModule } from "../product.module";
+import { ProductService } from "src/app/services/product.service";
+import { ActivatedRoute } from "@angular/router";
+import { PropertyModel } from "src/app/models/product.model";
+import { SolicitModel } from "src/app/models/solicitd.model";
+import { SolicitudService } from "src/app/services/solicitud.service";
+import { UserService } from "src/app/services/user.service";
+import * as moment from "moment";
 
 declare const SendSol: any;
 
 @Component({
-  selector: 'app-product-details',
-  templateUrl: './product-details.component.html',
-  styleUrls: ['./product-details.component.css']
+  selector: "app-product-details",
+  templateUrl: "./product-details.component.html",
+  styleUrls: ["./product-details.component.css"],
 })
 export class ProductDetailsComponent implements OnInit {
-
   client: string = "";
-  
-  constructor(private pdtService: ProductService, private route: ActivatedRoute, private sP: SolicitudService, private uService: UserService) { 
+
+  constructor(
+    private pdtService: ProductService,
+    private route: ActivatedRoute,
+    private sP: SolicitudService,
+    private uService: UserService
+  ) {
     let asesorinfo = this.uService.getUserInformation();
     this.client = asesorinfo.realm;
     console.log(this.client);
   }
 
-  product: PropertyModel ={
+  product: PropertyModel = {
     id: null,
     code: null,
     name: null,
@@ -42,25 +47,32 @@ export class ProductDetailsComponent implements OnInit {
     depto: null,
     city: null,
     encargado: null,
-    contact: null
-  }
+    contact: null,
+  };
 
   ngOnInit(): void {
     this.getProductInformation();
   }
 
-  getProductInformation():void{
+  getProductInformation(): void {
     let id = this.route.snapshot.params["id"];
-    this.pdtService.getProductById(id).subscribe(item => {
+    this.pdtService.getProductById(id).subscribe((item) => {
+      let today = new Date();
+      let day = `${today.getDate() + 1 < 10 ? "0" : ""}${today.getDate()}`;
+      let month = `${today.getMonth() + 1 < 10 ? "0" : ""}${
+        today.getMonth() + 1
+      }`;
+      let year = today.getFullYear();
+      let date = `${day}/${month}/${year}`;
       this.product = item;
-      this.solicitud.direccion= this.product.address;
-      this.solicitud.tipo= this.product.tipo;
-      this.solicitud.valor= this.product.price;
-      this.solicitud.fecha= "fecha";
-      this.solicitud.estado= "enviado";
-      this.solicitud.foto= this.product.image;
-      this.solicitud.asesor= this.product.encargado;
-    })
+      this.solicitud.direccion = this.product.address;
+      this.solicitud.tipo = this.product.tipo;
+      this.solicitud.valor = this.product.price;
+      this.solicitud.fecha = date.toString();
+      this.solicitud.estado = "enviado";
+      this.solicitud.foto = this.product.image;
+      this.solicitud.asesor = this.product.encargado;
+    });
   }
   /* -----------------------   SOLICITUD ----------------- */
   solicitud: SolicitModel = {
@@ -72,19 +84,15 @@ export class ProductDetailsComponent implements OnInit {
     fecha: null,
     estado: null,
     foto: null,
-    asesor: null
+    asesor: null,
   };
 
-
-  saveSolicitud(){
+  saveSolicitud() {
     this.solicitud.nombre = this.client;
     console.log(this.solicitud);
-     this.sP.saveNewSolcitud(this.solicitud).subscribe(item => {
-       SendSol("Se envió solicitud exitosamente");
+    this.sP.saveNewSolcitud(this.solicitud).subscribe((item) => {
+      SendSol("Se envió solicitud exitosamente");
       /* alert("Su solicitud ha sido enviada"); */
     });
   }
-  
-  
-
 }
