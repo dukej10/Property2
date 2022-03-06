338c0d999c2ebeabf2851392bef0ff2b77e0ab64
diegoduqqe@gmail.com <47634821+dukej10@users.noreply.github.com>
2022-03-05 23:12:39 -0500

    crear registro cuando se actualice una propiedad

--- client/src/app/product/admin/admin/product-editor/product-editor.component.ts
+++ client/src/app/product/admin/admin/product-editor/product-editor.component.ts
@@ -1,24 +1,32 @@
-import { Component, OnInit } from '@angular/core';
-import { ProductService } from 'src/app/services/product.service';
-import { ActivatedRoute, RouterLink, Router } from '@angular/router';
-import { CategoryModel } from 'src/app/models/category.model';
-import { ProductModule } from 'src/app/product/product.module';
-import { PropertyModel } from 'src/app/models/product.model';
-
+import { Component, OnInit } from "@angular/core";
+import { ProductService } from "src/app/services/product.service";
+import { ActivatedRoute, RouterLink, Router } from "@angular/router";
+import { CategoryModel } from "src/app/models/category.model";
+import { ProductModule } from "src/app/product/product.module";
+import { PropertyModel } from "src/app/models/product.model";
+import { ChangeService } from "../../../../services/change.service";
+import { UserService } from "../../../../services/user.service";
+import { ChangeModel } from "src/app/models/change.model";
 
 declare const editMessageProd: any;
-declare const eMessageProd: any;
+declare const createdMessageProd: any;
+declare const SendSol: any;
 
 @Component({
-  selector: 'app-product-editor',
-  templateUrl: './product-editor.component.html',
-  styleUrls: ['./product-editor.component.css']
+  selector: "app-product-editor",
+  templateUrl: "./product-editor.component.html",
+  styleUrls: ["./product-editor.component.css"],
 })
 export class ProductEditorComponent implements OnInit {
+  constructor(
+    private pService: ProductService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private chanService: ChangeService,
+    private user: UserService
+  ) {}
 
-  constructor(private pService: ProductService, private route: ActivatedRoute, private router: Router) { }
-
-  ngOnInit(){
+  ngOnInit() {
     this.searchProduct();
   }
 
@@ -40,20 +48,54 @@ export class ProductEditorComponent implements OnInit {
     depto: null,
     city: null,
     encargado: null,
-    contact: null
+    contact: null,
   };
 
-  searchProduct():void{
-    let id = this.route.snapshot.params["id"];
-    this.pService.getProductById(id).subscribe(item => {
-      this.product = item;
-    })
-  }
+  change: ChangeModel = {
+    id: null,
+    code: null,
+    name: null,
+    category: null,
+    image: null,
+    type: null,
+    manager: null,
+    date: null,
+  };
 
-  updateProduct(){
-    this.pService.updateProduct(this.product).subscribe(item => {
-      editMessageProd("Se actualizó la información del inmueble");
+  searchProduct(): void {
+    let id = this.route.snapshot.params["id"];
+    this.pService.getProductById(id).subscribe((item) => {
+      this.product = item;
     });
   }
 
+  updateProduct() {
+    this.pService.updateProduct(this.product).subscribe((item) => {
+      editMessageProd("Se actualizó la información del inmueble");
+    });
+    this.addChange();
+  }
+
+  addChange() {
+    console.log("FUNCIONÓ");
+    this.change.code = this.product.code;
+    this.change.name = this.product.name;
+    this.change.category = this.product.category;
+    this.change.image = this.product.image;
+    this.change.manager = this.user.getUserInformation().realm;
+    this.change.type = "Actualizar";
+    let today = new Date();
+    let day = `${today.getDate() + 1 < 10 ? "0" : ""}${today.getDate()}`;
+    let month = `${today.getMonth() + 1 < 10 ? "0" : ""}${
+      today.getMonth() + 1
+    }`;
+    let year = today.getFullYear();
+    let date = `${day}/${month}/${year}`;
+    this.change.date = date.toString();
+    console.log(this.user.getToken());
+    this.chanService.saveNewChange(this.change).subscribe((item) => {
+      createdMessageProd("Se creo el producto satisfactoriamente");
+      console.log(item);
+    });
+  }
 }
