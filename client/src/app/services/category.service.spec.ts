122b5b31c4464a9df3e2368dda89d0af9ce393d9
diegoduqqe@gmail.com <47634821+dukej10@users.noreply.github.com>
2022-03-07 13:09:38 -0500

    eliminar

--- client/src/app/services/category.service.spec.ts
+++ client/src/app/services/category.service.spec.ts
@@ -1,8 +1,8 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed } from "@angular/core/testing";
 
-import { CategoryService } from './category.service';
+import { CategoryService } from "./category.service";
 
-describe('CategoryService', () => {
+describe("CategoryService", () => {
   let service: CategoryService;
 
   beforeEach(() => {
@@ -10,7 +10,7 @@ describe('CategoryService', () => {
     service = TestBed.inject(CategoryService);
   });
 
-  it('should be created', () => {
+  it("should be created", () => {
     expect(service).toBeTruthy();
   });
 });
